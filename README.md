2091165a984907441b4878696d2280bf86f26aa9
jade695 <jade.alvarez@esme.fr>
2025-09-26 08:58:35 +0000

    debut atelier 2

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # C
 C project template with codespace container to start coding.
+
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+
+
+double wind_angle;
+
+    printf("Direction relative du vent [-3.14159265 à 3.14159265] radian : ");
+    scanf("%lf", & wind_angle); 
+
+    printf("Valeur saisie : %lf\n", wind_angle);
+
+    
+
+int wind_speed;
+    printf("vitesse vent [0 à 150] kts : ");
+    scanf("%f",& wind_speed);
+
+    printf("Valeur saisie : %f\n", wind_speed);
+
+int fuel;
+    printf("carburant disponible [20 à 350000] litres : ");
+    scanf("%f" ,& fuel);
+
+    printf("Valeur saisie : %f\n" , fuel);
+
+int consumption; 
+    printf("consommation [10 à 15000] litres/h : "); 
+    scanf("%f" , & consumption); 
+
+    printf("Valeur saisie : %f\n" , consumption); 
+
+int wind_surface; 
+    printf("surface alaire [5 à 900] m^2 : "); 
+    scanf("%f" , & wind_surface); 
+
+    printf("Valeur saisie : %f\n" , wind_surface); 
+     return 0;
+}
+
+
--- program.c
+++ program.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+
+
+double wind_angle;
+
+    printf("Direction relative du vent [-3.14159265 à 3.14159265] radian : ");
+    scanf("%lf", & wind_angle); 
+
+    printf("Valeur saisie : %lf\n", wind_angle);
+
+    
+
+int wind_speed;
+    printf("vitesse vent [0 à 150] kts : ");
+    scanf("%d",& wind_speed);
+
+    printf("Valeur saisie : %d\n", wind_speed);
+
+int fuel;
+    printf("carburant disponible [20 à 350000] litres : ");
+    scanf("%d" ,& fuel);
+
+    printf("Valeur saisie : %d\n" , fuel);
+
+int consumption; 
+    printf("consommation [10 à 15000] litres/h : "); 
+    scanf("%d" , & consumption); 
+
+    printf("Valeur saisie : %d\n" , consumption); 
+
+int wind_surface; 
+    printf("surface alaire [5 à 900] m^2 : "); 
+    scanf("%d" , & wind_surface); 
+
+    printf("Valeur saisie : %d\n" , wind_surface); 
+
+int pressure;
+    printf("pression ambiante [300 à 1050] hPa/mb : "); 
+    scanf("%d" , & pressure)
+     return 0;
 }
+  
